a628ad09953af9800d7bba7a029ad22bc42885d8
gagantmk06 <gagantmk06@gmail.com>
2022-11-14 15:24:23 +0530

    change1

---
+++ scalar.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 15:09:31 2022
+
+@author: gagan
+"""
+
+# Define scalars
+number_of_units = 50
+mass_of_sun = 1.989e30
+length_great_wall_of_china = 21196
+print(number_of_units)
+print(mass_of_sun)
+print(length_great_wall_of_china)
+
